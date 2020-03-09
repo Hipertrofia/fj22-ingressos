85fbd40691e7712d1555aecf36a5ec842dda9287
Hipertrofia <jansen@dcc.ufrj.br>
2020-03-09 20:44:12 -0300

    COLOCANDOPREÇONASALAEFILME

--- src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
+++ src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
@@ -12,7 +12,7 @@ public class GerenciadorDeSessao {
 	private List<Sessao> sessoesDaSala;
 
 	public GerenciadorDeSessao(List<Sessao> sessoesDaSala) {
-		this.sessoesDaSala = this.sessoesDaSala;
+		this.sessoesDaSala = sessoesDaSala;
 	}
 
 	public boolean cabe(Sessao sessaoNova) {
@@ -23,44 +23,34 @@ public class GerenciadorDeSessao {
 	}
 
 	private boolean terminaAmanha(Sessao sessao) {
-
-		LocalDateTime terminoSessaoNova = getInicioSessaoComDiaDeHoje(sessao);
-
-		LocalDateTime terminoSessaaoNova = getTerminoSessaoComDiaDeHoje(sessao);
+		LocalDateTime terminoSessaoNova = getTerminoSessaoComDiaDeHoje(sessao);
 		LocalDateTime ultimoSegundoDeHoje = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
-
 		if (terminoSessaoNova.isAfter(ultimoSegundoDeHoje)) {
 			return true;
 		}
-
 		return false;
 	}
 
 	private boolean horarioIsConflitante(Sessao sessaoExistente, Sessao sessaoNova) {
 		LocalDateTime inicioSessaoExistente = getInicioSessaoComDiaDeHoje(sessaoExistente);
 		LocalDateTime terminoSessaoExistente = getTerminoSessaoComDiaDeHoje(sessaoExistente);
-		LocalDateTime inicioSessaoNova = getTerminoSessaoComDiaDeHoje(sessaoNova);
+		LocalDateTime inicioSessaoNova = getInicioSessaoComDiaDeHoje(sessaoNova);
 		LocalDateTime terminoSessaoNova = getTerminoSessaoComDiaDeHoje(sessaoNova);
-
 		boolean sessaoNovaTerminaAntesDaExistente = terminoSessaoNova.isBefore(inicioSessaoExistente);
 		boolean sessaoNovaComecaDepoisDaExistente = terminoSessaoExistente.isBefore(inicioSessaoNova);
-
 		if (sessaoNovaTerminaAntesDaExistente || sessaoNovaComecaDepoisDaExistente) {
 			return false;
 		}
-
 		return true;
 	}
 
 	private LocalDateTime getInicioSessaoComDiaDeHoje(Sessao sessao) {
 		LocalDate hoje = LocalDate.now();
-
 		return sessao.getHorario().atDate(hoje);
 	}
 
 	private LocalDateTime getTerminoSessaoComDiaDeHoje(Sessao sessao) {
 		LocalDateTime inicioSessaoNova = getInicioSessaoComDiaDeHoje(sessao);
-
 		return inicioSessaoNova.plus(sessao.getFilme().getDuracao());
 	}
 }
--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.validacao;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -24,8 +25,8 @@ public class GerenciadorDeSessaoTest {
 
 	@Before
 	public void preparaSessoes() {
-		this.rogueOne = new Filme("Rogue	One", Duration.ofMinutes(120), "SCI-FI");
-		this.sala3D = new Sala("Sala	3D");
+		this.rogueOne = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
+		this.sala3D = new Sala("Sala	3D", BigDecimal.TEN);
 		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueOne, sala3D);
 		this.sessaoDasTreze = new Sessao(LocalTime.parse("13:00:00"), rogueOne, sala3D);
 		this.sessaoDasDezoito = new Sessao(LocalTime.parse("18:00:00"), rogueOne, sala3D);
@@ -65,7 +66,7 @@ public class GerenciadorDeSessaoTest {
 	public void garanteQueDeveNaoPermitirUmaSessaoQueTerminaNoProximoDia() {
 		List<Sessao> sessoes = Collections.emptyList();
 		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
-		Sessao sessaoQueTerminaAmanha = new Sessao(LocalTime.parse("23:00:00"), rogueOne,sala3D);
+		Sessao sessaoQueTerminaAmanha = new Sessao(LocalTime.parse("23:00:00"), rogueOne, sala3D);
 		Assert.assertFalse(gerenciador.cabe(sessaoQueTerminaAmanha));
 	}
 }
